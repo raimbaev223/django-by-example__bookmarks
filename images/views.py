59c4b35f5c739b824bd3e8073993a81b3a4e8845
raimbaev223 <raimbaev.223@gmail.com>
2021-03-18 12:45:07 +0600

    18.03

--- images/views.py
+++ images/views.py
@@ -8,6 +8,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import ImageCreateForm
 from .models import Image
 from common.decorators import ajax_required
+from actions.utils import create_action
+
 
 @login_required
 def image_list(request):
@@ -32,13 +34,6 @@ def image_list(request):
                   {'section': 'images', 'images': images})
 
 
-
-
-
-
-
-
-
 @ajax_required
 @login_required
 @require_POST
@@ -50,6 +45,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
@@ -78,6 +74,7 @@ def image_create(request):
             # assign current user to the item
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'bookmarked image', new_item)
             messages.success(request, 'Image added successfully')
 
             # redirect to new created item detail view
